21d3724102e9e90008654af726b574fa619f7418
pabloo <pablomx@gmail.com>
2011-02-12 11:38:16 -0700

    updated: assembly version

--- fluentAOP.Core/Properties/AssemblyInfo.cs
+++ fluentAOP.Core/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System;
 [assembly: ComVisible(false)]
 [assembly: Guid("9f9e0011-4b62-45e1-9d79-49dcaf5e5503")]
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyVersion("1.0.14.0")]
-[assembly: AssemblyFileVersion("1.0.14.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
